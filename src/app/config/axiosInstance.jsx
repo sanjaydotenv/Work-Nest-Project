38c1df581b8065670ccf023155c732023956264c
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-08-16 21:27:49 +0530

    auth complete

--- src/app/config/axiosInstance.jsx
+++ src/app/config/axiosInstance.jsx
@@ -4,3 +4,21 @@ export const axiosInstnace = axios.create({
   baseURL: "https://team-sync-backend-n78w.onrender.com/api",
   withCredentials: true,
 });
+
+axiosInstnace.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRes = error.config;
+    if (error.response.status === 401 && !originalRes._retry) {
+      originalRes._retry = true;
+
+      try {
+        await axiosInstnace.get("/auth/get-accessToken");
+        return axiosInstnace(originalRes)
+      } catch (error) {
+        window.location.href("/");
+        return Promise.reject(error);
+      }
+    }
+  },
+);
